9840c1b57010e186a7fd9759e7cfb1dfffce8d65
Sumaiya Akter <shampa.themefisher@gmail.com>
2021-11-01 13:57:24 +0600

    Updated gulpfile.js

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,3 +1,10 @@
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
+
 // Preloader js    
 $(window).on('load', function () {
   $('.preloader').fadeOut(100);
